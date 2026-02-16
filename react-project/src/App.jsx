692eb3fd92df3bbe750f04731ef4e0292648306c
yAlnaeem <yalnaeem94@gmail.com>
2026-02-16 09:38:26 +0100

    save changes

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,22 +1,52 @@
-import { useState } from "react";
 import "./App.css";
+import reactImage from '/src/assets/react.svg';
+
+const reactQuote = [
+  "Coding like poetry should be short and concise",
+  "It’s not a bug; it’s an undocumented feature.",
+  "First, solve the problem. Then, write the code.",
+];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
 function Header() {
+  const description = reactQuote[genRandomInt(reactQuote.length - 1)];
   return (
     <header>
       <h1>React Essentials</h1>
+      <p>{description}</p>
     </header>
   );
 }
 
-function App() {
-  const [count, setCount] = useState(0);
 
+function coreConcept(){
+	return(
+		<li>
+			<img src="" alt="" />
+			<h3>Title</h3>
+			<p>Description</p>
+		</li>
+	)	
+}
+
+
+
+
+
+
+
+
+function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
-        <h2>Time to get start!</h2>
+		
+        <h1>Time to get started!</h1>
+        <p>React concepts you will need for all your projects.</p>
       </main>
     </div>
   );
